33f25d24642bf0d35ab8cf9d534dd95557d53df4
Abhishek jha <abhishekjhagzb2001@gmail.com>
2023-07-01 12:39:06 +0530

    problem5.js
    Find Occurance of a target value in given array;

---
+++ problem5.js
@@ -0,0 +1,10 @@
+16. Find Occurance of a target value in given array;
+arr= [19, 22, 18, 19, 16, 18, 19, 21, 24];
+target = 19;
+
+// solution
+
+arr=[19,22,18,19,16,18,19,21,24];
+arr1=arr.filter(e=>e==19);
+count=arr1.length;
+3
